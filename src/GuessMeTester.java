ada89bc5af3f7c7adca4b564a830a906dcecf486
makovviec <radoslaw.krysinski@gmail.com>
2023-07-15 10:16:25 +0200

    First Commit

---
+++ src/GuessMeTester.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessMeTester {
+    static Random rnd = new Random();
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        final var LENGTH = 10;
+        System.out.println("Welcome!");
+        System.out.println("This is a challenge called \"Guess Number\".");
+        System.out.println("############################################");
+        System.out.println("Please choose a number length you wish to guess.");
+        int length = scanner.nextInt();
+        scanner.nextLine();
+        int generatedNumber = generateNumber(length);
+        System.out.println(hashUnknownDigits(generatedNumber));
+        System.out.println(generatedNumber);
+    }
+
+    private static String hashUnknownDigits(int generatedNumber) {
+        String txt = Integer.toString(generatedNumber);
+        return "*".repeat(txt.length());
+    }
+
+    private static int generateNumber(int length) {
+        var multiply = 10;
+        if (length == 1) {
+            return rnd.nextInt(multiply);
+        } else if (length > 2) {
+            int min = (int) Math.pow(multiply, length - 1);
+            int max = (int) Math.pow(multiply, length) - 1;
+            return rnd.nextInt(max - min + 1) + min;
+        } else {
+            return 0;
+        }
+    }
+}
